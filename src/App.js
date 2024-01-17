f71a3c364ab0c9a22d6ac473501204000b2c7384
M0stafa123 <jodat.ahmed135@gmail.com>
2024-01-17 16:17:03 +0200

    readme photo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
 function App() {
   return (
@@ -33,8 +33,15 @@ function App() {
       Learn More */}
 
       <div class="attribution">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-        Coded by <a href="#">Your Name Here</a>.
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
       </div>
     </div>
   );
