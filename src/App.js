014eff5173434bb6e048cb980ad82182c289d1ef
kirito0san <jodat.ahmedsyed@gmail.com>
2024-01-17 17:44:35 +0200

    add bg for app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 
 import Header from "./Header";
 import Main from "./Main";
-
+import top from "./images/bg-top.svg"
+import bottom from "./images/bg-bottom.svg"
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-no-repeat min-h-[100vh] ">
+      <img className=" -z-10 absolute -top-[50px] -right-[200px] md:right-0  " src={top} alt="" />
+      <img className=" -z-10 absolute w-0 md:w-auto bottom-0 left-0 " src={bottom} alt="" />
       <Main />
       <Header />
-      <div class="attribution">
+      <div class="w-[90%] text-[#a3a8f0] mt-5 mx-auto">
         Challenge by{" "}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
@@ -17,7 +20,7 @@ function App() {
         >
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Your Name Here</a>.
+        . Coded by <a href="#">Mostafa & Ahmed</a>.
       </div>
     </div>
   );
